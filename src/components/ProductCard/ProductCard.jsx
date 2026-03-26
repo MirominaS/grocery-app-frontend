99bf9894147b49b0143d043bc8dde110aff98060
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-03-26 16:29:42 +0530

    product card component created

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './ProductCard.css'
+
+const ProductCard = ({product}) => {
+  return (
+    <div className= 'product-card' >
+      <img
+        src={product.image_url|| "/image.png "}
+        alt={ product.name }
+        className= 'product-image'
+      />
+      <div className= 'product-info' >
+        <h3 className= 'product-name' >{ product.name }</h3>
+        <p className= 'product-category' >{ product.category }</p>
+        <p className= 'product-price'>${ product.price }</p>
+      </div>
+      <button className= 'add-to-cart-btn'>Add to Cart</button>
+    </div>
+  )
+}
+
+export default ProductCard
