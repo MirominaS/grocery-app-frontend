7653816add83c712802167422ffe0348f5da47d1
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-03-27 10:39:38 +0530

    api call for getAll orders for admin page

--- src/api/orderApi.js
+++ src/api/orderApi.js
@@ -6,9 +6,20 @@ export const placeOrder = async (orderData) => {
         method: 'POST',
         headers: {'Content-Type' : 'application/json'},
         body: JSON.stringify(orderData)
-       })
+       });
        return await response.json()
     } catch (error) {
-        console.log("Error Placing order:",error)
+        console.log("Error Placing order:",error);
+    }
+}
+
+export const getAllOrders = async () => {
+    try {
+        const response = await fetch(`${BASE_URL}/orders`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log("Error fetching orders:", error)
+        return {success:false, message:"Fetching error"}
     }
 }
